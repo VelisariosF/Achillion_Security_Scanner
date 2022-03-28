61017c771c97ae875a540e6f745ce649d5176b45
VelisariosF <fafoutis@csd.auth.gr>
2022-03-29 00:16:07 +0300

    project updated

--- app/src/main/java/com/androidapp/achillion_security_scanner/Config.java
+++ app/src/main/java/com/androidapp/achillion_security_scanner/Config.java
@@ -1,8 +1,6 @@
 package com.androidapp.achillion_security_scanner;
 
-/**
- * Created by Belal on 10/30/2015.
- */
+
 public class Config {
     public static final String EMAIL ="info.achillioncompany@gmail.com";
     public static final String PASSWORD ="pataros16";
